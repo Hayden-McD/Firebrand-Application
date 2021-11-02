581dce594ff842bd4a49ec319e75edaa6f7ccf83
Hayden-McD <mcdohr2@student.op.ac.nz>
2021-11-02 22:27:43 +1300

    Removed most styling from about me page, added links to navbar and added content to about me page.

--- firebrand-application/src/App.js
+++ firebrand-application/src/App.js
@@ -1,20 +1,49 @@
 import './App.css';
+import { Navbar, Nav, NavItem, NavLink } from 'reactstrap';
 
 function App() {
   return (
     <div class="container">
-      <div class="title">
-        My Firebrand summer internship application
+      <div class="nav-container">
+        <Navbar>
+          <Nav>
+            <NavItem>
+              <NavLink href="/">
+                About me
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">
+                My study
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">
+                Skills And Attributes
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">
+                Why I want The Internship
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">
+                What I hope To Achieve
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
       </div>
-
-      {/* Put pages here. One page for each question */}
-
-
-      {/* buttons for selecting page. when button is pressed current page will become hidde, and new page will become visable */}
-      <div class="page-select-buttons"> 
-        <button class="previous"> Previous </button>
-        <button class="next"> Next </button>
-
+      <div class="body-container">
+        <h2>Hayden McDowall</h2>
+        <h3>A little bit about me:</h3>
+        <h4>
+          I am 21 years old, Dunedin born and raised. I still live at home with my amazing mum, my Dog Jazz and my Bearded Dragon Larry.
+          Over the years I have had many interests from Air Cadets to playing with all things electronic. I have gained a particular interest in crypto mining, 3d-printing,
+          Gaming, Mountain biking and cars to name a few. I always like to keep myself busy by working on small projects or expanding my knowledge.
+          I chose to study I.T as I felt this aligned with my areas of interest.
+        </h4>
       </div>
     </div>
   );
