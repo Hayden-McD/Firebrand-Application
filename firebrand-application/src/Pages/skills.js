e005eaff0f886a02284d032fc6877ed73f60053b
Hayden-McD <mcdohr2@student.op.ac.nz>
2021-11-04 10:52:21 +1300

    Added styling for skills container and edited skills and attributes

--- firebrand-application/src/Pages/skills.js
+++ firebrand-application/src/Pages/skills.js
@@ -7,18 +7,19 @@ const Skills = () => {
       <div class="page-title">
         <p>My special skills and attributes</p>
       </div>
-      <div class="paper-container-left">
+      <div class="skills-container">
         <h3>
           Skills & Attributes:
         </h3>
-        <ListGroup class="papers-list">
+        <ListGroup class="skills-list">
           <ListGroup.Item>Fast Learner</ListGroup.Item>
           <ListGroup.Item>Always wanting to expand my knowledge</ListGroup.Item>
           <ListGroup.Item>Passionate about IT</ListGroup.Item>
           <ListGroup.Item>Able to work induvidially or in a group</ListGroup.Item>
-          <ListGroup.Item>I always strive to do my best</ListGroup.Item>
+          <ListGroup.Item>Aim for excellence</ListGroup.Item>
           <ListGroup.Item>Laid-back but serious when need </ListGroup.Item>
-          <ListGroup.Item>Able to work under pressure</ListGroup.Item>
+          <ListGroup.Item>Work well under pressure</ListGroup.Item>
+          <ListGroup.Item>Commited worker</ListGroup.Item>
         </ListGroup>
       </div>
     </>
