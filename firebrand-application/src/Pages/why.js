672fcadbb756cd8fa9d83a02c7671aeb47227f79
Hayden-McD <mcdohr2@student.op.ac.nz>
2021-11-04 12:28:59 +1300

    Added why page content and styled to center text and moved down from page title

--- firebrand-application/src/Pages/why.js
+++ firebrand-application/src/Pages/why.js
@@ -5,6 +5,22 @@ const Why = () => {
       <div class="page-title">
         <p>Why I want the summer internship</p>
       </div>
+      <div class="why-container">
+        <div class="why-paragraph">
+          <p>
+            A few months ago I saw Firebrand advertising for three internship roles, but I was sad to see that none of those roles aligned with my study and plans for future work.
+            One night I was sitting in bed scrolling through Instagram and I saw the advertisement for a developer role at Firebrand
+            and a smile came to my face knowing that it was now a possibility for me to become a Firebrander.
+          </p>
+        </div>
+
+        <div class="why-paragraph">
+          <p>
+            It would be an amazing oppertunity to work for Firebrand as I have always admired the positive culture and have been following the company on social media for many years,
+            and would love to join your awesome team.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
